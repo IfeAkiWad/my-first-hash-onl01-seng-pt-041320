f6a8987ef4aaed4106748f92a5001b613ae6da52
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-19 21:46:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,12 +15,9 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-  }
+  shipping_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3}
   shipping_manifest["muskets"] = 2 # add 2 muskets to the shipping_manifest hash below
+    puts ["muskets"]
   shipping_manifest["gun powder"] = 4 # add 4 gun powder to the shipping_manifest hash below
-    puts shipping_manifest              # return the shipping_manifest hash below
+    puts shipping_manifest["gun powder"]              # return the shipping_manifest hash below
 end
